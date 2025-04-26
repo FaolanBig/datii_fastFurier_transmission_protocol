dae12cb26f0105bd571df6d275ab59ed53d240f3
FaolanBig <Faolan.Big@web.de>
2025-04-26 21:12:14 +0200

    adding classes

---
+++ Encoder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datii_fastFurier_transmission_protocol
+{
+    internal class Encoder
+    {
+    }
+}
---
+++ VarHold.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datii_fastFurier_transmission_protocol
+{
+    internal static class VarHold
+    {
+
+    }
+    public static class VarHold_public
+    {
+
+    }
+}
