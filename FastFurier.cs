8b9050faabd52f139f0459e1bc80c94388e14c49
FaolanBig <Faolan.Big@web.de>
2025-04-26 23:25:05 +0200

    adding AGPL hint in files

--- FastFurier.cs
+++ FastFurier.cs
@@ -1,5 +1,24 @@
 ﻿// This code is part of the datii_fastFurier_transmission_protocol project.
 
+/*  datii (datii_fastFurier_transmission_protocol) lets you transmit files via sound from one computer to another.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    You can contact me via
+        email: faolan.big@web.de
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
