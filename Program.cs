f3ebbae735606e71103239109e77202c0083bc24
FaolanBig <Faolan.Big@web.de>
2025-04-26 21:03:10 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace datii_fastFurier_transmission_protocol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
