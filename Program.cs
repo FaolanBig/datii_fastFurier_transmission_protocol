418eb82b7cbb49ae28c6579a71e28b12b6b69669
FaolanBig <Faolan.Big@web.de>
2025-04-28 21:22:13 +0200

    adding basic input handling

--- Program.cs
+++ Program.cs
@@ -1,10 +1,52 @@
-﻿namespace datii_fastFurier_transmission_protocol
+﻿using System.IO.Enumeration;
+
+namespace datii_fastFurier_transmission_protocol
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length != 2)
+            {
+                Console.WriteLine("error: wrong input");
+                Console.WriteLine("usage: datii send file_to_send");
+                Console.WriteLine("       datii receive folder_to_receive_to");
+                Environment.Exit(1);
+            }
+            if (args[0] == "send")
+            {
+                string fileToSend = args[1];
+                if (!File.Exists(fileToSend))
+                {
+                    Console.WriteLine("error: file does not exist");
+                    Environment.Exit(1);
+                }
+            }
+            else if (args[0] == "receive")
+            {
+                string folderToReceiveTo = args[1];
+                if (!Directory.Exists(folderToReceiveTo))
+                {
+                    Console.WriteLine("error: folder does not exist");
+                    Console.WriteLine($"create directory {folderToReceiveTo}? (y/n)");
+                    if (Console.ReadKey().Key == ConsoleKey.Y)
+                    {
+                        Directory.CreateDirectory(folderToReceiveTo);
+                    }
+                    else
+                    {
+                        Console.WriteLine("abort");
+                        Environment.Exit(1);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("error: wrong input");
+                Console.WriteLine("usage: datii send file_to_send");
+                Console.WriteLine("       datii receive folder_to_receive_to");
+                Environment.Exit(1);
+            }
         }
     }
 }
